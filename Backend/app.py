7ca34f1f1b08449d1e32cabc25c5874eadf76eb4
dinhthigaamer <longle336699@gmail.com>
2025-11-30 10:50:12 +0700

    merge frontend, astar, map

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
-# Xác định đường dẫn tuyệt đối đến thư mục Frontend
+# Đường dẫn tuyệt đối đến thư mục Frontend
 frontend_path = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '../Frontend'))
 
-# Tạo app, chỉ định template_folder
-app = Flask(__name__, template_folder=frontend_path)
+# Tạo app, template_folder = Frontend, static_folder = Frontend luôn
+app = Flask(
+    __name__,
+    template_folder=frontend_path,  # HTML files
+    static_folder=frontend_path,    # CSS, JS, images trực tiếp trong Frontend
+    static_url_path='/'       # URL prefix
+)
 
 
 @app.route('/')
 def home():
-    return render_template('user.html')  # Chỉ cần tên file
+    return render_template('index.html')
 
 
 @app.route('/admin')
